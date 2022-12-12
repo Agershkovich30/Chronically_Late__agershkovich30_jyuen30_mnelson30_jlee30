87696652aec635c38adce1c78dcc9269442cef96
julia <jlee30@stuy.edu>
2022-12-12 02:10:50 -0500

    Added in-line comments

--- test/app.py
+++ test/app.py
@@ -6,9 +6,7 @@ CLIENT_ID="6a0dedd845844c95a73948003cc241b7"
 CLIENT_SECRET="62c8b70c2c684632b447ae65b1f4dd0e"
 SECRET_KEY="seret-key"
 REDIRECT_URL="http://localhost:5000/redirect"
-LOCAL_URL="http://localhost:5000"
 ACCESS_TOKEN_URL='https://accounts.spotify.com/api/token'
-TOKEN_CODE = "BQDs01Ihut6uZ9Z9alod7ft9uBn_85hTd57TsNOMEypwPjZILXZgzlxlmrk4-T85wSjzItPX0wsai31g3C9f88mwZQvTYA629PadhDMZGxkL_LvpM9bIeCpww7jss2I_ruJ_Z89eeP9TBee_hfMlnyDGxuakl6_3bWch0SrxAt0S2xXdKFUW-IpvElRR5PcSjsyhaOrgvvg1qODKw4Y"
 
 app = Flask(__name__)
 app.secret_key = SECRET_KEY
@@ -18,27 +16,33 @@ app.config['SESSION_COOKIE_NAME'] = 'Julia Cookie'
 def index():
     return redirect('/login')
 
+#Redirects you to the Spotify authorize request page.
 @app.route('/login', methods=["GET","POST"])
 def login():
-    scope = 'user-read-private user-read-email'
+    scope = 'user-read-private user-read-email' #Determines the scope of information you are requesting access to.
+    # Gets the URL for the Spotify authorize request page.
     req = requests.get(f'https://accounts.spotify.com/authorize?client_id={CLIENT_ID}&response_type=code&redirect_uri={REDIRECT_URL}&scope={scope}')
     temp = req.url
     return redirect(temp)
 
 @app.route('/redirect', methods=["GET","POST"])
 def process():
+    #Stores the code that you got from the login page when you accepted the connection to spotify
     code = request.args.get('code')
     state = request.args.get('state')
+    #This is the body of the post request you will make later.
     form = {
         'grant_type': 'authorization_code',
         'code': f'{code}',
         'redirect_uri': f'{REDIRECT_URL}'
     }
+    #Spotify requires base64 encoding for client_id and client_secret when providing it in the header.
     client_creds = f'{CLIENT_ID}:{CLIENT_SECRET}'
     client_creds_b64 = base64.b64encode(client_creds.encode())
     headers = {
         'Authorization': f"Basic {client_creds_b64.decode()}"
     }
+    #POST request using the code obtained from logging in. Most importantly, returns the token.
     req = requests.post(ACCESS_TOKEN_URL,data=form,headers=headers)
     return req.json()
 
